ce14925f13aaeff4503a96049d2d7309ad3d299a
Darth-Arminius <arminpudic@yahoo.co.uk>
2023-08-17 10:50:01 +0100

    Add advanced E2E test

--- components/page.tsx
+++ components/page.tsx
@@ -1,34 +1,41 @@
-import Header from './header'
-import Meta from './meta'
+import { useEffect } from 'react';
+import Header from './header';
+import Meta from './meta';
 
-const Page = ({ children }) => (
-  <div className="main">
-    <Meta />
-    <Header />
+const Page = ({ children }) => {
+  useEffect(() => {
+    document.title = 'Home';
+  }, []);
 
-    <div className="page">{children}</div>
+  return (
+    <div className='main'>
+      <Meta />
+      <Header />
 
-    <style jsx>{`
-      .main {
-        width: 85%;
-        margin: auto;
-        padding: 10px 0 0 0;
-      }
+      <div className='page'>{children}</div>
 
-      .page {
-        color: #828282;
-        background: #fff;
-        padding: 3px 10px;
-      }
-
-      @media (max-width: 750px) {
+      <style jsx>{`
         .main {
-          padding: 0;
-          width: auto;
+          width: 85%;
+          margin: auto;
+          padding: 10px 0 0 0;
         }
-      }
-    `}</style>
-  </div>
-)
 
-export default Page
+        .page {
+          color: #828282;
+          background: #fff;
+          padding: 3px 10px;
+        }
+
+        @media (max-width: 750px) {
+          .main {
+            padding: 0;
+            width: auto;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Page;
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -14,4 +14,28 @@ describe('Login journey', () => {
     }); // Asserting that the url is correct
     cy.title().should('eq', 'Login'); // Asserting that the page title is correct
   });
+
+  it('should log me into the app successfully when completing the login journey', () => {
+    // WHEN I click on the Login link
+    cy.get('[data-testid="header-login-link"]').click();
+    cy.wait(1000); // These waits are purely for visual purposes
+
+    // AND I input "username" in the username input box
+    cy.get('[data-testid="login-username-input"]').type('username');
+    cy.wait(1000);
+
+    // AND I input "password" in the password input box
+    cy.get('[data-testid="login-password-input"]').type('password');
+    cy.wait(1000);
+
+    // AND I click the Login button
+    cy.get('[data-testid="login-cta"]').click();
+    cy.wait(1000);
+
+    // THEN it should navigate to the Home page
+    cy.location().should((location) => {
+      expect(location.pathname).to.equal('/');
+    });
+    cy.title().should('eq', 'Home');
+  });
 });
